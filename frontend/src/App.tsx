1ef97e6e5690329a643830f3e4a443e9ec75b4e8
Marvelstalwart <Marvelstalwart@gmail.com>
2023-11-09 18:29:38 +0100

    Folder structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <header>
+      TS AMAZONA
+    </header>
+    <main>
+
+    </main>
+    <footer>
+      All rights reserved
+    </footer>
+   </div>
+  )
+}
+
+export default App
